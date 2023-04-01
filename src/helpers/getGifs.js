0af49feb72dcf568f70c7e2f931ad0606886a35a
Ronald Cedeno <ronald199928@hotmail.com>
2023-03-31 21:08:02 -0500

    first commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+
+export const getGifs = async(category) => {
+    const url = `https:\\api.giphy.com/v1/stickers/search?q=${encodeURI(category)}&limit=10&api_key=mKwYB4qZuLOr1F3nRDsjq5TXdHnWuVEe`
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url
+        }
+    });
+
+    return gifs;
+}
