0b1e4cf66ae26fbc07410d110ddb86ea2c9608d1
Ronald Cedeno <ronald199928@hotmail.com>
2023-04-01 10:03:07 -0500

    fix api call

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 
 export const getGifs = async(category) => {
-    const url = `https:\\api.giphy.com/v1/stickers/search?q=${encodeURI(category)}&limit=10&api_key=mKwYB4qZuLOr1F3nRDsjq5TXdHnWuVEe`
+    const url = `https://api.giphy.com/v1/stickers/search?q=${encodeURI(category)}&limit=10&api_key=mKwYB4qZuLOr1F3nRDsjq5TXdHnWuVEe`
     const resp = await fetch(url);
     const {data} = await resp.json();
 
